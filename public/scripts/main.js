b3074a531d90beaa9e0c47308bc1d57629e8fe84
Gustavo <gustgouvea@hotmail.com>
2021-12-23 10:49:37 -0300

    alterações js modal

---
+++ public/scripts/main.js
@@ -0,0 +1,29 @@
+import Modal from './modal.js'
+
+const modal = Modal()
+
+//Pegar todos o botões que existe com a classe check
+const checkButtons = document.querySelectorAll(".actions a.check")
+
+checkButtons.forEach(button => {
+    //adicionar a escuta
+    button.addEventListener ("click", event => {
+        //Abrir modal
+        modal.open()
+    })
+})
+
+
+/* Quando o botão delete for clicado ele abre a modal */ 
+const deleteButton = document.querySelectorAll(".actions a.delete")
+
+deleteButton.forEach(button => {
+    button.addEventListener("click", event =>{
+        modal.open()
+    })
+})
+
+
+
+
+
